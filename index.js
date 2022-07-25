5f46e81ffada38cb7daaf9ecac94219ea9018e01
XiaoPan <2489964425@qq.com>
2022-07-25 17:35:09 +0800

    优化代码

--- index.js
+++ index.js
@@ -19,8 +19,72 @@ module.exports.clear = async () => {
     await db.writeFile([]);
 };
 
-module.exports.showAll = async () => {
-    let taskList = await db.readFile();
+// 已完成 未完成 修改 删除
+const taskActionMap = {
+    finish: (taskList, index) => {
+        taskList[index].done = true;
+        db.writeFile(taskList);
+    },
+    undone: (taskList, index) => {
+        taskList[index].done = false;
+        db.writeFile(taskList);
+    },
+    deleteTask: (taskList, index) => {
+        taskList.splice(index, 1);  // splice从index开始删除1个，slice(start, end)从start，截取到end不包含end，split(',')以,切割字符串成数组
+        db.writeFile(taskList);
+    },
+    update: (taskList, index) => {
+        inquirer
+          .prompt({
+              type: "input",
+              name: "taskTitle",
+              message: "输入任务标题"
+          }).then(({taskTitle}) => {
+            taskList[index].taskTitle = taskTitle;
+            db.writeFile(taskList);
+        });
+    }
+};
+
+// 询问接下来操作
+function askForAction(taskList, index) {
+    inquirer
+      .prompt({
+          type: "list",
+          name: "method",
+          message: "请选择操作",
+          choices: [
+              {name: "退出", value: "exit"},
+              {name: "已完成", value: "finish"},
+              {name: "未完成", value: "undone"},
+              {name: "改标题", value: "update"},
+              {name: "删除", value: "deleteTask"}
+          ]
+      })
+      .then(({method}) => {
+          // 执行不同操作
+          taskActionMap[method](taskList, index);
+      });
+}
+
+// 新增任务
+function addTask(taskList) {
+    inquirer
+      .prompt({
+          type: "input",
+          name: "taskTitle",
+          message: "输入任务标题"
+      }).then(({taskTitle}) => {
+        taskList.push({
+            taskTitle,
+            done: false
+        });
+        db.writeFile(taskList);
+    });
+}
+
+// 打印所有任务
+function printTasks(taskList) {
     inquirer
       .prompt({
             type: "list",
@@ -36,64 +100,17 @@ module.exports.showAll = async () => {
       )
       .then(({index}) => {
           let i = parseInt(index);
-          if (i >= 0) {
-              // 询问接下来操作
-              inquirer
-                .prompt({
-                    type: "list",
-                    name: "method",
-                    message: "请选择操作",
-                    choices: [
-                        {name: "退出", value: "exit"},
-                        {name: "已完成", value: "finish"},
-                        {name: "未完成", value: "undone"},
-                        {name: "改标题", value: "update"},
-                        {name: "删除", value: "delete"}
-                    ]
-                })
-                .then(({method}) => {
-                    switch (method) {
-                        case "finish":
-                            taskList[index].done = true;
-                            db.writeFile(taskList);
-                            break;
-                        case "undone":
-                            taskList[index].done = false;
-                            db.writeFile(taskList);
-                            break;
-                        case "delete":
-                            taskList.splice(index, 1);  // splice从index开始删除1个，slice(start, end)从start，截取到end不包含end，split(',')以,切割字符串成数组
-                            db.writeFile(taskList);
-                            break;
-                        case "update":
-                            inquirer
-                              .prompt({
-                                  type: "input",
-                                  name: "taskTitle",
-                                  message: "输入任务标题"
-                              }).then(({taskTitle}) => {
-                                taskList[index].taskTitle = taskTitle;
-                                db.writeFile(taskList);
-                            });
-                            break;
-                    }
-                });
-          } else if (i === -2) {
-              // 添加
-              inquirer
-                .prompt({
-                    type: "input",
-                    name: "taskTitle",
-                    message: "输入任务标题"
-                }).then(({taskTitle}) => {
-                  taskList.push({
-                      taskTitle,
-                      done: false
-                  });
-                  db.writeFile(taskList);
-              });
-          }
+          // 询问操作 askForAction
+          if (i >= 0) askForAction(taskList, i);
+          // 添加
+          if (i === -2) addTask(taskList)
       });
+}
+
+module.exports.showAll = async () => {
+    let taskList = await db.readFile();
+    // 展示任务 printTasks
+    printTasks(taskList);
 };
 
 
